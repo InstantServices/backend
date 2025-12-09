20985d64418ec9abbd8934cf3bc58bf1676182b1
vijayalakshmiGogineni <vijayalakshmigogineni2005@gmail.com>
2025-12-09 17:05:48 +0530

    removed lombok use in OfferRequest

--- src/main/java/com/instantservices/backend/dto/OfferRequest.java
+++ src/main/java/com/instantservices/backend/dto/OfferRequest.java
@@ -2,14 +2,44 @@ package com.instantservices.backend.dto;
 
 
 
-import lombok.Getter;
-import lombok.Setter;
 
-@Getter
-@Setter
+
+
 public class OfferRequest {
     private Long taskId;
     private double proposedCommission;
     private String message;
     private String expectedTime;
+
+    public Long getTaskId() {
+        return taskId;
+    }
+
+    public void setTaskId(Long taskId) {
+        this.taskId = taskId;
+    }
+
+    public double getProposedCommission() {
+        return proposedCommission;
+    }
+
+    public void setProposedCommission(double proposedCommission) {
+        this.proposedCommission = proposedCommission;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getExpectedTime() {
+        return expectedTime;
+    }
+
+    public void setExpectedTime(String expectedTime) {
+        this.expectedTime = expectedTime;
+    }
 }
